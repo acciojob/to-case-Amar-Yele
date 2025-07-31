e4d7b2c9d271fd69b02a8581a50ef46395876f61
Amar-Yele <110736798+Amar-Yele@users.noreply.github.com>
2025-07-31 18:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
   // write your code here
+	let output = "";
+
+	let uc = text.toUpperCase();
+	let lc = text.toLowerCase();
+	output = lc.concat("-"+uc);
+	return output;
 }
 
 // DO not change the code below
